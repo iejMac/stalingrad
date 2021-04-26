7fe81a0a4b33bec9472891678fdb4f6b7913f226
iejMac <kilianmaciej6@gmail.com>
2021-04-26 03:08:16 -0700

    complete operator set

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import variable
 from variable import Variable
-from variable import mul, add
+from variable import mul, add, neg, inv
 
 a = Variable(2, name="a")
 b = Variable(3, name="b")
 
 # c = a*b
-c = mul(a, b)
+c = a*b
 # d = a+b
-d = add(a, b)
+d = a-b
 
 # e = c*d = a*b*(a+b) = a^2b + b^2a = 12 + 18 = 30
-e = mul(c, d)
+e = c*d
 
-print(e)
-print(e.value)
-print(e.local_grads[c])
+f = e/d
+
+print(f.value)
--- variable.py
+++ variable.py
@@ -1,4 +1,3 @@
-
 class Variable:
   def __init__(self, value, name="", local_grads={}):
     self.value = value
@@ -6,7 +5,27 @@ class Variable:
     self.name = name
   def __repr__(self):
     return f'Variable "{self.name}"'
+  def __neg__(self):
+    return neg(self)
+  def __inv__(self):
+    return inv(self)
+  def __add__(self, x):
+    return add(self, x)
+  def __sub__(self, x):
+    return add(self, neg(x))
+  def __mul__(self, x):
+    return mul(self, x)
+  def __truediv__(self, x):
+    return mul(self, inv(x))
 
+def neg(x: Variable) -> Variable:
+  return Variable(x.value * (-1), f"(-{x.name})", {
+    x: -1
+  })
+def inv(x: Variable) -> Variable:
+  return Variable(1 / x.value, f"(1/{x.name})", {
+    x: -1 / ((x.value)**2)
+  })
 def add(x: Variable, y: Variable) -> Variable:
   return Variable(x.value + y.value, f"({x.name}+{y.name})", {
     x: 1,
