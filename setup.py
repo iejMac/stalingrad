d60a70d5cf35ea75a579635dffd41cc873081490
iejMac <kilianmaciej6@gmail.com>
2021-05-05 13:01:51 +0200

    add numpy

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-	name="stalingrad",
-	version="0.1.0",
-	author="Maciej Kilian",
-	author_email="kilianmaciej6@gmail.com",
-	description="our autograd engine",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/iejMac/stalingrad",
-	packages=find_packages(),
-	python_requires='>=3.8',
+  name="stalingrad",
+  version="0.1.0",
+  author="Maciej Kilian",
+  author_email="kilianmaciej6@gmail.com",
+  description="our autograd engine",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/iejMac/stalingrad",
+  install_requires=["numpy"],
+  packages=find_packages(),
+  python_requires='>=3.8',
 )
