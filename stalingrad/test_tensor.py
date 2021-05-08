cdd294dffbb0227b13a5a0b99a0cbb2d53d8858c
iejMac <kilianmaciej6@gmail.com>
2021-05-08 11:41:33 +0200

    adding CI for tensor diff

--- stalingrad/test_tensor.py
+++ stalingrad/test_tensor.py
@@ -3,33 +3,30 @@ from tensor import Tensor
 
 np.random.seed(0)
 
-X = [np.array([[1, 0, 1, 0]]),
-     np.array([[0, 0, 1, 1]]),
-     np.array([[1, 0, 0, 0]]),
-     np.array([[0, 1, 0, 1]])]
-Y = [10.0, 3.0, 8.0, 5.0]
+X_train = np.array([[[1, 0, 1, 0]],
+                   [[0, 0, 1, 1]],
+                   [[1, 0, 0, 0]],
+                   [[0, 1, 0, 1]]])
+
+Y_train = np.array([10.0, 3.0, 8.0, 5.0])
 
 w1 = Tensor(np.random.random((4, 5)), name = "w1")
 w2 = Tensor(np.random.random((5, 1)), name = "w2")
 
-epochs = 10
+epochs = 100
 lr = 0.01
 
+# Train:
 for e in range(epochs):
-  for i in range(len(X)):
-    x = Tensor(X[i], name = "x")
-    y = Tensor(np.array([[Y[i]]]), name = "y")
+  for i in range(len(X_train)):
+    x = Tensor(X_train[i], name = "x")
+    y = Tensor(np.array([[Y_train[i]]]), name = "y")
 
     # Forward pass:
-    h1 = x@w1
-    h1.name = "h1"
-
-    h2 = h1@w2
-    h2.name = "h2"
-
+    h1 = x @ w1
+    h2 = h1 @ w2
     loss = (h2 - y)**2
-    loss.name = "loss"
-    
+
     # Backward pass:
     loss.backprop()
 
@@ -39,5 +36,23 @@ for e in range(epochs):
 
     loss.zero_grad()
 
-print(w1)
-print(w2)
+# Test:
+X_test = np.array([[[1, 1, 1, 0]],
+                   [[0, 0, 0, 1]],
+                   [[1, 1, 0, 0]],
+                   [[0, 0, 1, 0]]])
+
+Y_test = np.array([14.0, 1.0, 12.0, 2.0])
+
+cumulative_loss = 0.0
+
+for i, x in enumerate(X_test):
+  x = Tensor(x, name="x")
+  y = Tensor(Y_test[i], name="y")
+
+  h1 = x @ w1
+  h2 = h1 @ w2
+  loss = (h2 - y)**2
+
+  cumulative_loss += loss.data
+print(cumulative_loss)
