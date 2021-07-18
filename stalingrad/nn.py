functions: Adding reshape ; nn: fixing NLL loss ; tensor: allowing distribution axis selection for softmax

stalingrad/nn.py
@@ -50,8 +50,9 @@ class MSE(Loss):
     return (prediction - target)**2
 
 class NLL(Loss):
-  def __init__(self, reduction=None, reduce_axis=(0, 1)):
-    # reduce axis is (0, 1) because we want to reduce across batch dimension and class dimension
+  def __init__(self, reduction=None, reduce_axis=0):
     super().__init__(reduction, reduce_axis)
   def forward(self, prediction, target):
-    return target * prediction.log() * (-1.0)
+    sum_axis = list(range(len(prediction.shape)))
+    sum_axis.remove(self.reduce_axis)
+    return (target * prediction.log() * (-1.0)).sum(axis=tuple(sum_axis))

stalingrad/tensor.py
@@ -48,9 +48,11 @@ class Tensor:
   def sigmoid(self):
     e_x = self.exp()
     return e_x / (e_x + 1)
-  def softmax(self):
+  def softmax(self, dist_axes=(1,)):
+    axis = (dist_axes,) if isinstance(dist_axes, int) else dist_axes
+    shape = [1 if ax in axis else self.shape[ax] for ax in range(len(self.shape))]
     e_x = self.exp()
-    return e_x / e_x.sum()
+    return e_x / (e_x.sum(axis=dist_axes).reshape(shape=shape))
 
 
 class Function:

stalingrad/test_nn.py
@@ -4,16 +4,16 @@ from stalingrad.tensor import Tensor
 
 import optim
 
-x = Tensor(np.random.uniform(size=(1, 20)), requires_grad=False)
-labs = Tensor(np.zeros(10).reshape((1, 10)), requires_grad=False)
+x = Tensor(np.random.uniform(size=(2, 20)), requires_grad=False)
+labs = Tensor(np.zeros((2, 10)), requires_grad=False)
 labs.data[0][3] = 1.0
+labs.data[1][5] = 1.0
 
-nll_loss = nn.NLL(reduction="mean", reduce_axis=None)
+nll_loss = nn.NLL(reduction="sum")
 
 d1 = nn.Linear(20, 10)
 op = optim.SGD(d1.parameters(), 0.1)
 y = d1(x).softmax()
-print(y.sum())
 
 print(y)
 print(labs)