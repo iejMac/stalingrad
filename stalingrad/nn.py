b2145af3f7b0bda917874cba03d045cec4d38b61
iejMac <kilianmaciej6@gmail.com>
2021-07-27 17:53:42 +0200

    nn: adding Conv2d Module

--- stalingrad/nn.py
+++ stalingrad/nn.py
@@ -31,6 +31,44 @@ class Linear(Module):
       result += self.bias
     return result
 
+
+class Conv2d(Module):
+  def __init__(self, in_channels, out_channels, kernel_size=3, stride=(1, 1), padding="valid", groups=1, use_bias=True):
+    self.use_bias = use_bias
+    self.shape = (out_channels, in_channels, kernel_size, kernel_size)
+    self.stride, self.padding, self.groups = stride, padding, groups
+  
+    self.kernels = Tensor(np.random.uniform(-1., 1., size=self.shape)/np.sqrt(np.prod(self.shape)).astype(np.float32))
+    self.bias = None
+
+  def forward(self, x):
+    result = x.conv2d(self.kernels, stride=self.stride, groups=self.groups)
+    if self.use_bias:
+      if self.bias is None: # initialize bias after finding out output dim
+        self.bias = Tensor(np.random.uniform(-1., 1., result.shape))
+      result += self.bias
+    return result
+    
+'''
+  Notes for Conv2d module:
+
+    padding (zero padding):
+      int - symmetric padding on both axes
+      tuple - padding[0] zeros before, padding[1] zeros after on both axes
+      list of tuples - padding[0] for x axis, padding[1] for y axis
+
+    padding = (padding, padding) if isinstance (padding, int) else padding
+    padding = [padding, padding] if isinstance(padding[0], int) else padding
+    func.save_tensors(stride, padding.copy())
+
+    np_pad = [padding.pop() if len(padding) > 0 else (0, 0) for _ in range(len(x.shape))]
+    np_pad.reverse()
+
+    padded_x = np.pad(x, np_pad)
+
+
+'''
+
 # -= Losses =-
 
 class Loss(Module):
--- stalingrad/test_nn.py
+++ stalingrad/test_nn.py
@@ -1,10 +1,18 @@
-import numpy as np
 import nn
+import numpy as np
 from stalingrad.tensor import Tensor
 
-imgs = Tensor(np.ones((5, 2, 11, 11)), requires_grad=True)
-kern = Tensor(np.ones((4, 1, 3, 3)), requires_grad=True)
 
-out = imgs.conv2d(kern, groups=2)
+imgs = Tensor(np.ones((10, 1, 28, 28)), requires_grad=True)
 
-out.backward()
+
+conv1 = nn.Conv2d(1, 4, 3)
+conv2 = nn.Conv2d(4, 8, 3)
+conv3 = nn.Conv2d(8, 1, 3)
+
+out = conv1(imgs).relu()
+out = conv2(out).relu()
+out = conv3(out).relu()
+
+print(out.shape)
+
