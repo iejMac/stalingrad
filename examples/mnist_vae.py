2a6124df11270203c333dc7c220aedc676be5437
iejMac <kilianmaciej6@gmail.com>
2021-08-26 01:56:28 +0000

    examples/mnist_vae: linear example that works well

--- examples/mnist_vae.py
+++ examples/mnist_vae.py
@@ -8,39 +8,65 @@ from stalingrad.utils import train_module, fetch_mnist, save_module, load_module
 
 class Encoder(nn.Module):
   def __init__(self, latent_dim):
-    self.conv1 = nn.Conv2d(1, 96, 4, stride=2)
-    self.conv2 = nn.Conv2d(96, 96, 3, stride=1)
-    self.conv3 = nn.Conv2d(96, 4, 3, stride=1)
-    self.lin = nn.Linear(324, 2 * latent_dim)
+    self.conv1 = nn.Conv2d(1, 96, 4, stride=1)
+    self.conv2 = nn.Conv2d(96, 96, 4, stride=1)
+    self.conv3 = nn.Conv2d(96, 96, 4, stride=1)
+    self.conv4 = nn.Conv2d(96, 96, 3, stride=1)
+    self.conv5 = nn.Conv2d(96, 1, 3, stride=1)
+    self.lin = nn.Linear(225, 2 * latent_dim)
   def forward(self, x):
     x = self.conv1(x).relu()
     x = self.conv2(x).relu()
     x = self.conv3(x).relu()
+    x = self.conv4(x).relu()
+    x = self.conv5(x).relu()
     x = x.reshape(shape=(x.shape[0], -1))
     x = self.lin(x)
+    return x
+
+class EncoderLin(nn.Module):
+  def __init__(self, latent_dim):
+    self.lin1 = nn.Linear(784, latent_dim ** 2)
+    self.lin2 = nn.Linear(latent_dim ** 2, 2 * latent_dim)
+  def forward(self, x):
+    x = self.lin1(x).relu()
+    x = self.lin2(x)
     return x
 
 class Decoder(nn.Module):
   def __init__(self, latent_dim):
     self.lin = nn.Linear(latent_dim, 324)
-    self.conv1T = nn.ConvTranspose2d(4, 96, 3, stride=1)
+    self.conv1T = nn.ConvTranspose2d(4, 96, 3, stride=2)
     self.conv2T = nn.ConvTranspose2d(96, 128, 3, stride=1)
-    self.conv3T = nn.ConvTranspose2d(128, 1, 4, stride=2)
+    self.conv3T = nn.ConvTranspose2d(128, 128, 3, stride=1)
+    self.conv4T = nn.ConvTranspose2d(128, 128, 3, stride=1)
+    self.conv5T = nn.ConvTranspose2d(128, 1, 4, stride=1)
   def forward(self, x):
     x = self.lin(x)
     x = x.reshape(shape=(-1, 4, 9, 9))
     x = self.conv1T(x).relu()
     x = self.conv2T(x).relu()
-    x = self.conv3T(x).sigmoid()
+    x = self.conv3T(x).relu()
+    x = self.conv4T(x).relu()
+    x = self.conv5T(x).sigmoid()
+    return x
+
+class DecoderLin(nn.Module):
+  def __init__(self, latent_dim):
+    self.lin1 = nn.Linear(latent_dim, latent_dim ** 2)
+    self.lin2 = nn.Linear(latent_dim ** 2, 784)
+  def forward(self, x):
+    x = self.lin1(x).relu()
+    x = self.lin2(x).sigmoid()
     return x
 
 class MnistVAE(nn.Module):
   def __init__(self, latent_dim):
     self.latent_dim = latent_dim
-    self.encoder = Encoder(latent_dim)
-    self.decoder = Decoder(latent_dim)
+    self.encoder = EncoderLin(latent_dim)
+    self.decoder = DecoderLin(latent_dim)
   def forward(self, x):
-    x = x.reshape(shape=(-1, 1, 28, 28))
+    # x = x.reshape(shape=(-1, 1, 28, 28))
     lat = self.encoder(x)
     mu, logvar = lat[:, :self.latent_dim], lat[:, self.latent_dim:]
 
@@ -61,16 +87,18 @@ def VAELoss(preds, targets):
   print(kl_loss, reconstruction_loss)
   return reconstruction_loss + kl_loss
   
-X_train, _, X_test, _ = fetch_mnist(flatten=False, one_hot=True)
-X_train, X_test = np.expand_dims(X_train, 1) / 255.0, np.expand_dims(X_test, 1) / 255.0 
+# X_train, _, X_test, _ = fetch_mnist(flatten=False, one_hot=True)
+X_train, _, X_test, _ = fetch_mnist(flatten=True, one_hot=True)
+# X_train, X_test = np.expand_dims(X_train, 1) / 255.0, np.expand_dims(X_test, 1) / 255.0 
+X_train, X_test = X_train / 255.0, X_test / 255.0 
 train = True
 
 if __name__ == "__main__":
   if train:
-    lr = 1e-2
-    steps = 100
+    lr = 1e-3
+    steps = 1500
     latent_dim = 20
-    batch_size = 200
+    batch_size = 256
 
     mod = MnistVAE(latent_dim)
     optimizer = optim.Adam(mod.parameters(), learning_rate=lr)
