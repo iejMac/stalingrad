fee56629fff796ed3a96b66312023a4c7cbbd2f3
iejMac <kilianmaciej6@gmail.com>
2023-04-13 02:42:39 -0700

    interesting how important MultiHead is...

--- lm_test.py
+++ lm_test.py
@@ -9,37 +9,42 @@ from stalingrad.data import fetch_shakespeare, get_batch
 
 # Define model:
 # TODO: implement
-class Attention(nn.Module):
-  def __init__(self, d_model, context_length):
+class MultiHeadAttention(nn.Module):
+  def __init__(self, embed_dim, num_heads):
     super().__init__()
-    self.d_model = d_model
-    self.context_length = context_length
+    self.embed_dim = embed_dim
+    self.num_heads = num_heads
+    self.head_dim = embed_dim // num_heads
+    assert self.head_dim * num_heads == self.embed_dim, "embed_dim must be divisible by num_heads"
 
-    self.w_attn = nn.Linear(d_model, d_model * 3, use_bias=False)
-    self.w_proj = nn.Linear(d_model, d_model, use_bias=False)
+    self.w_attn = nn.Linear(embed_dim, embed_dim * 3, use_bias=False)
+    self.w_proj = nn.Linear(embed_dim, embed_dim, use_bias=False)
 
 
   def forward(self, x):
     B, T, C = x.shape
 
     emb_attn = self.w_attn(x)
-    q = emb_attn[:, :, :self.d_model]
-    k = emb_attn[:, :, self.d_model:self.d_model*2]
-    v = emb_attn[:, :, self.d_model*2:self.d_model*3]
+    q = emb_attn[:, :, :self.embed_dim]
+    k = emb_attn[:, :, self.embed_dim:self.embed_dim*2]
+    v = emb_attn[:, :, self.embed_dim*2:self.embed_dim*3]
 
-    # TODO: make this multihead, need to do some slicing/transposinghere
+    q = q.reshape(shape=(B, T, self.num_heads, self.head_dim)).transpose(order=(0, 2, 1, 3))
+    k = k.reshape(shape=(B, T, self.num_heads, self.head_dim)).transpose(order=(0, 2, 1, 3))
+    v = v.reshape(shape=(B, T, self.num_heads, self.head_dim)).transpose(order=(0, 2, 1, 3))
 
     # TODO: is * (1/x) faster than /x ??? test it out, might be interesting
-    scores = (q @ k.transpose(order=(0, 2, 1))) / math.sqrt(k.shape[-1])
-    # TODO: attn masks here
+    scores = (q @ k.transpose(order=(0, 1, 3, 2))) / math.sqrt(k.shape[-1])
 
-    scores = scores.softmax(dist_axes=(1,))
+    # TODO: attn masks here
+    scores = scores.softmax(dist_axes=(3,))
 
     # TODO: attn dropout
 
-    # TODO: need to reassemble attention heads once we add those
     y = scores @ v
     # TODO: need to reassemble attention heads once we add those
+    y = y.transpose(order=(0, 2, 1, 3)).reshape(shape=(B, T, C))
+
 
     y = self.w_proj(y)
 
@@ -56,18 +61,19 @@ class Transformer(nn.Module):
 train_data, val_data = fetch_shakespeare(data_dir="data/shakespeare")
 
 # training stuff
-steps = 10
+steps = 20
 batch_size = 4
 # TODO: think about rounding up to 50304 like karpathy https://github.com/karpathy/nanoGPT/blob/a82b33b525ca9855d705656387698e13eb8e8d4b/train.py#L144
 vocab_size = 50257 
 
 # model init and params
-context_length = 8
-embed_dim = 32 # 768
+context_length = 7
+embed_dim = 64 # 768
+num_heads = 4
 
-model = Attention(
-  d_model=embed_dim,
-  context_length=context_length,
+model = MultiHeadAttention(
+  embed_dim=embed_dim,
+  num_heads=num_heads,
 )
 
 # TODO: for weight sharing I'm trying init lm_head then tok_emb = lm_head.weight
@@ -84,7 +90,7 @@ model_params["lm_head.weight"] = lm_head.weight
 model_params["pos_emb"] = pos_emb
 
 loss_func = nn.NLL(reduction="mean")
-opt = optim.Adam(model_params, learning_rate=1e-3)
+opt = optim.Adam(model_params, learning_rate=1e-2)
 
 
 # TODO: make init better because things might need to be initialized differently
@@ -92,8 +98,9 @@ opt = optim.Adam(model_params, learning_rate=1e-3)
 # - residual projections
 # - maybe token embs???
 
+bx, by = get_batch(train_data, context_length, batch_size)
 for step in range(steps):
-  bx, by = get_batch(train_data, context_length, batch_size)
+  # bx, by = get_batch(train_data, context_length, batch_size)
 
   # TODO: just make CrossEntropyLoss, this is getting annoying
   by_oh = np.eye(vocab_size)[by]
