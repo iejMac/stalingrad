0980a8219cc88423308534c1ab4202b0df3253f0
Maciej Kilian <kilianmaciej6@gmail.com>
2023-04-20 15:17:16 -0700

    GPU: generalize to unary_op + add backward_unary_op (nicer code) + implement Log and Exp on GPU (#12)
    * GPU: generalize to unary_op
    
    * forward log and exp works'
    
    * nicer code
    
    * remove line

--- stalingrad/ops/ops_gpu.py
+++ stalingrad/ops/ops_gpu.py
@@ -13,8 +13,7 @@ def init_gpus():
     # TODO: for now lets do 1 GPU
     # TODO: in the future for more devices we can maintain a context and queue for each
     # and pass index info to GPUBuffer
-    # devices = devices[:1]
-    devices = [devices[5]]
+    devices = [devices[0]]
     cl_ctx = cl.Context(devices)
     cl_queue = cl.CommandQueue(cl_ctx)
 init_gpus()
@@ -41,37 +40,59 @@ def empty_buf(shape, dtype=np.float32):
   return buf
 
 
+def unary_op(code, x):
+  result = empty_buf(x.shape, x.dtype)
+  unary_op_kernel = """
+    __kernel void unary_op(__global const float *input, __global float *output) {
+      int gid = get_global_id(0);
+      float x = input[gid];
+      output[gid] = """+code+""";
+    }
+  """
+  prg = cl.Program(cl_ctx, unary_op_kernel).build()
+  prg.unary_op(cl_queue, x.shape, None, x.buf, result.buf)
+  return result
+def backward_unary_op(code, x, upstream_grad):
+  result_grad = empty_buf(x.shape, x.dtype)
+  # TODO: for now lets keep grad in numpy array but need to make a PR that changes this
+  grad_buf = GPUBuffer(upstream_grad)
+  backward_unary_op_kernel = """
+    __kernel void backward_unary_op(__global const float *input, __global const float *upstream_gradient, __global float *output) {
+      int gid = get_global_id(0);
+      float x = input[gid];
+      float up_grad = upstream_gradient[gid];
+      output[gid] = """+code+""";
+    }
+  """
+  prg = cl.Program(cl_ctx, backward_unary_op_kernel).build()
+  prg.backward_unary_op(cl_queue, x.shape, None, x.buf, grad_buf.buf, result_grad.buf)
+  np_grad = result_grad.toCPU()
+  return np_grad
+
+
 # TODO: THIS SHIT IS SO SLOW
 class ReLU(Function):
   def forward(func, x):
     func.save_tensors(x)
-    result = empty_buf(x.shape, x.dtype)
+    return unary_op("max(0.0f, x)", x)
 
-    kernel_code = """
-    __kernel void relu(__global const float *input, __global float *output) {
-        int gid = get_global_id(0);
-        output[gid] = max(0.0f, input[gid]);
-    }
-    """
-    prg = cl.Program(cl_ctx, kernel_code).build()
-    prg.relu(cl_queue, x.shape, None, x.buf, result.buf)
-    return result
+  def backward(func, passed_grad): # upstream_gradient[gid] * (input[gid] > 0.0f)
+    x = func.saved_tensors[0]
+    return backward_unary_op("up_grad * (x > 0.0f)", x, passed_grad)
+
+class Log(Function):
+  def forward(func, x):
+    func.save_tensors(x)
+    return unary_op("log(x)", x)
   def backward(func, passed_grad):
     x = func.saved_tensors[0]
-    result_grad = empty_buf(x.shape, x.dtype)
-    # TODO: for now lets keep grad in numpy array but need to make a PR that changes this
-    grad_buf = GPUBuffer(passed_grad)
+    return backward_unary_op("up_grad * (1 / x)", x, passed_grad)
 
-    kernel_code = """
-    __kernel void relu_backward(__global const float *input, __global const float *upstream_gradient, __global float *grad_output) {
-        int gid = get_global_id(0);
-        grad_output[gid] = upstream_gradient[gid] * (input[gid] > 0.0f);
-    }
-    """
-    prg = cl.Program(cl_ctx, kernel_code).build()
-    prg.relu_backward(cl_queue, x.shape, None, x.buf, grad_buf.buf, result_grad.buf)
-
-    np_grad = result_grad.toCPU()
-    return np_grad
-
-
+class Exp(Function):
+  def forward(func, x):
+    ret = unary_op("exp(x)", x)
+    func.save_tensors(ret) # d(e^x)/dx = e^x
+    return ret
+  def backward(func, passed_grad):
+    e_x = func.saved_tensors[0]
+    return backward_unary_op("up_grad * x", e_x, passed_grad)
